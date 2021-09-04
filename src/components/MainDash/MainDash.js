21adb7d29c9e1a55111fa17845a9713e336aa681
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-09-04 14:03:30 -0400

    changed header layout slightly

--- src/components/MainDash/MainDash.js
+++ src/components/MainDash/MainDash.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "./components/Hero";
+import Bestsellers from "./components/Bestsellers";
 import "../../styles/MainDash.scss";
 
 function MainDash(props) {
   return (
     <div>
       <Hero />
+      <Bestsellers />
     </div>
   );
 }
--- src/components/MainDash/components/Hero.js
+++ src/components/MainDash/components/Hero.js
@@ -2,10 +2,12 @@ import React from "react";
 
 function Hero(props) {
   return (
-    <div className="hero-wrap">
-      <div className="text-wrap">
-        <h1 class="hero-text">All Natural Handmade Soaps</h1>
-        <button className="btn btn-light shopBtn btn-lg">Shop Now</button>
+    <div className="hero-wrap-wrap">
+      <div className="hero-wrap">
+        <div className="text-wrap">
+          <h1 class="hero-text">All Natural Handmade Soaps</h1>
+          <button className="btn btn-light shopBtn btn-lg">Shop Now</button>
+        </div>
       </div>
     </div>
   );
